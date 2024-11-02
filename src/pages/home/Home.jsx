4db37859dcc4e54b434fcba7066d23e7d699278f
Sipul <nayaksipul143@gmail.com>
2024-11-02 18:24:09 +0530

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Banner from './Banner'
+import Topseller from './Topsellers'
+import Recommened from './Recommened'
+import News from './News'
+const Home = () => {
+  return (
+    <>
+    <Banner/>
+    <Topseller/>
+    <Recommened/>
+    <News/>
+    </>
+  )
+}
+
+export default Home
